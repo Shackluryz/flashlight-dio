cd60abbe413f0079e4d84c46577cd280b58bd237
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-06-14 08:04:37 -0300

    [ UPDATE ] Refatoração de Código

--- App.js
+++ App.js
@@ -1,13 +1,5 @@
-/* eslint-disable eol-last */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
-import {
-  View,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-} from 'react-native';
+import {View, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import Torch from 'react-native-torch';
 import RNShake from 'react-native-shake';
 
@@ -16,26 +8,35 @@ const App = () => {
 
   const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
 
-  useEffect(()=>{
+  useEffect(() => {
     Torch.switchState(toggle);
   }, [toggle]);
 
   useEffect(() => {
-    const subscription = RNShake.addListener(() => handleChangeToggle());
-    
+    const subscription = RNShake.addListener(() => {
+      setToggle(oldToggle => !oldToggle);
+    });
     return () => subscription.remove();
   }, []);
-  
+
   return (
     <View style={toggle ? style.containerLight : style.container}>
       <TouchableOpacity onPress={handleChangeToggle}>
-        <Image 
-          style={toggle ? style.lightingOn : style.lightingOff} 
-          source={toggle ? require('./assets/icons/eco-light.png') : require('./assets/icons/eco-light-off.png')} 
+        <Image
+          style={toggle ? style.lightingOn : style.lightingOff}
+          source={
+            toggle
+              ? require('./assets/icons/eco-light.png')
+              : require('./assets/icons/eco-light-off.png')
+          }
         />
-        <Image 
-          style={style.dioLogo} 
-          source={toggle ? require('./assets/icons/logo-dio.png') : require('./assets/icons/logo-dio-white.png')} 
+        <Image
+          style={style.dioLogo}
+          source={
+            toggle
+              ? require('./assets/icons/logo-dio.png')
+              : require('./assets/icons/logo-dio-white.png')
+          }
         />
       </TouchableOpacity>
     </View>
