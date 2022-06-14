85f6182c17ce29ebbacb10192cd4c6343c2ed198
Lucas Rodrigues <43302793+Shackluryz@users.noreply.github.com>
2022-06-14 07:37:04 -0300

    [ FIX ] Correção de uma falha no uso do RNShake

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
+/* eslint-disable eol-last */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
 import {
   View,
   StyleSheet,
   Image,
-  TouchableOpacity
+  TouchableOpacity,
 } from 'react-native';
 import Torch from 'react-native-torch';
 import RNShake from 'react-native-shake';
@@ -11,16 +14,14 @@ import RNShake from 'react-native-shake';
 const App = () => {
   const [toggle, setToggle] = useState(false);
 
-  const handleChangeToggle = () => setToggle((oldToggle) => !oldToggle);
+  const handleChangeToggle = () => setToggle(oldToggle => !oldToggle);
 
   useEffect(()=>{
     Torch.switchState(toggle);
-  },[toggle]);
+  }, [toggle]);
 
   useEffect(() => {
-    const subscription = RNShake.addListener(() => {
-      setToggle((oldToggle) => !oldToggle);
-    });
+    const subscription = RNShake.addListener(() => handleChangeToggle());
     
     return () => subscription.remove();
   }, []);
@@ -39,7 +40,7 @@ const App = () => {
       </TouchableOpacity>
     </View>
   );
-}
+};
 
 export default App;
 
@@ -48,31 +49,31 @@ const style = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   containerLight: {
     flex: 1,
     backgroundColor: 'white',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   lightingOn: {
     resizeMode: 'contain',
     alignSelf: 'center',
     width: 150,
-    height: 150
+    height: 150,
   },
   lightingOff: {
     resizeMode: 'contain',
     alignSelf: 'center',
     tintColor: 'white',
     width: 150,
-    height: 150
+    height: 150,
   },
   dioLogo: {
     resizeMode: 'contain',
     alignSelf: 'center',
     width: 250,
-    height: 250
-  }
+    height: 250,
+  },
 });
